788d21456408faaca5d65e99e00ed3f276227854
Theo Kent <theo.kent@givenergy.co.uk>
2024-11-04 20:47:15 +0000

    feat: in-memory user list

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [UserModule, AuthModule, UsersModule],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,41 +1,61 @@
-
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
-
-export type User = {
-    userId: number,
-    username: string,
-    password: string
-}
+import {User} from "./entities/user.entity"
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
-  ];
+    private readonly users: User[] = [
+        {
+          userId: 1,
+          username: 'admin',
+          password: 'password',
+          role: 'admin'
+        },
+      ];
+      async create(createUserDto: CreateUserDto): Promise<User> {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(createUserDto.password, saltRounds);
+        
+        const newUser: User = {
+            userId: this.users.length + 1,
+            username: createUserDto.username,
+            password: hashedPassword,
+            role: createUserDto.role
+        };
+        
+        this.users.push(newUser);
+        return newUser;
+    }
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
-  }
+    findAll(): User[] {
+        return this.users;
+    }
 
-  async createUser(username: string, password: string): Promise<User> {
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-  
-    const user = new this.userModel({
-      username,
-      password: hashedPassword,
-    });
-  
-    return user.save();
-  }
+    async findOne(username: string): Promise<User | undefined> {
+        return this.users.find(user => user.username === username);
+    }
+
+    async update(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+        const user = this.users.find(user => user.userId === id);
+        if (!user) throw new NotFoundException(`User with ID ${id} not found`);
+
+        if (updateUserDto.username) user.username = updateUserDto.username;
+        if (updateUserDto.password) {
+            const saltRounds = 10;
+            user.password = await bcrypt.hash(updateUserDto.password, saltRounds);
+        }
+
+        return user;
+    }
+
+    remove(id: number): string {
+        const index = this.users.findIndex(user => user.userId === id);
+        if (index === -1) throw new NotFoundException(`User with ID ${id} not found`);
+
+        this.users.splice(index, 1);
+        return `User #${id} removed successfully`;
+    }
 }
+
