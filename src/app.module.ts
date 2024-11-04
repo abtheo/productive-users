974e00c99d20a521786a96af22a71a76719a3049
Theo Kent <theo.kent@givenergy.co.uk>
2024-11-04 23:10:42 +0000

    feat: products resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersController } from 'src/users/users.controller';
 import { UsersService } from 'src/users/users.service';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, ProductsModule],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
